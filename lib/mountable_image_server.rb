50fb7e660f64e8098b1fce8319238a384dca0084
David Strauß <david@strauss.io>
2016-08-02 10:28:13 +0200

    Prepare for release

--- lib/mountable_image_server.rb
+++ lib/mountable_image_server.rb
@@ -3,6 +3,8 @@ require "dry-configurable"
 require "mountable_image_server/version"
 
 module MountableImageServer
+  require "mountable_image_server/server"
+
   extend Dry::Configurable
 
   setting :sources, []
